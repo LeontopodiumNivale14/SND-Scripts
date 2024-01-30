43f80b4a4f6b1ed94bfe37676a352ef232621f8d
xicemanx95 <103514618+xicemanx95@users.noreply.github.com>
2024-01-30 18:38:29 -0500

    Update Alexander Farming.lua

--- SND Scripts/Alexander Farming.lua
+++ SND Scripts/Alexander Farming.lua
@@ -2,18 +2,24 @@
 This is meant to be used for Alexander - The Burden of the Father (NORMAL NOT SAVAGE)
 It's setup to where you should be able to loop it as many time as you want, and be able to farm mats for GC seals
 Known classes to work: MCH | SMN | RDM | BLM | BRD
+
+Plugins that are used are:
+-> Visland (for pathing)
+-> Pandora (Setting "Open Chest")
+-> RotationSolver
 ]]
+
+-- How many loops do you wanna do
+NumberofLoops = 1
 
 --Visland Loops
 Alex_Start = "H4sIAAAAAAAACk2PW2vDMAyF/4ueTXCyNJv9VrYO8tDdIVvHKKbVqKG2Rq3uQsh/nxJctjcd6dPRUQ83LiBYmO/xe12vH0FB534+yEdOYF97uKPk2VME28MzWF3oqrmYmaY2Cl7AlrqY1ea8qhWswJrCNGdaV4MoitheyYKCB7f1R3ErCxFL+sSAkadJGxkPbsOd591tpv/3cjgJlXb0dZpIGnF7d/uEf/gUsVSwCMSnwy1jyOV8IrK4P2LiXI/GnfPjt9lxVNd0uKS4zZ8LNjaffMClcHp4G34B1YIamzkBAAA="
 Alex_Chest = "H4sIAAAAAAAACuWS22oDIRRF/+U8G1FHZ0bfQi+Qh/RGYNqUEqSxRIhaMqYXhvn3OonpQPsH7dvZx81muzgdXGlnQMF0az5WfLUABI3+fA3WxxbUYwc3obXRBg+qg3tQDEtRcVlyBA+gKMEl4VIwBEtQE4HrmtSM9kkGb2bnoDgXCO702u5THMUEwTy8GWd8BJXEzEez08+xsXFzPfh/7HK71KrdhPfTS6qT0l70tjWj/dCRIrhwIZpTVDQuj9ODI4vbvWljnofgRts4Jg7qMuzOgl/nr5PjcmGdmScf6dEvMBOCKZGEympkIzgvhTyykVhIWovqX7IpcCHSoaQGGYwsWPF9NJVgNWV/H8xT/wWlwgmYbwMAAA=="
 
--- How many loops do you wanna do
-NumberofLoops = 10
-
 -- Values that are needed for the whole script
 CurrentLoop = 1 -- This is just the loop counter itself, keeps tracks of how many you've done.
-DutyCounter = 0
+DutyCounter = 0 
+DutyFail = 0
 
 ::LoopTest::
 if NumberofLoops >= CurrentLoop then
@@ -48,14 +54,14 @@ elseif DutyCounter == 0 then
 
   yield("/pcall ContentsFinder True 12 0")
   DutyCounter = DutyCounter + 1
-  yield("/wait 3.0")
+  yield("/wait 2.0")
 elseif DutyCounter == 1 then
   yield("/visland resume")
   yield("/visland stop")
   yield("/dutyfinder") --Open Duty Finder
   yield("/wait 1.0")
   yield("/pcall ContentsFinder True 12 0") --Duty Load
-  yield("/wait 3.0") 
+  yield("/wait 2.0") 
 elseif DutyCounter == 2 then
   yield("/echo Hmm... it seems like this has failed, so going to reset it")
   DutyCounter = 0 
@@ -114,13 +120,18 @@ CurrentLoop = CurrentLoop + 1
 yield("/echo Leaving the instance")
 yield("/wait 0.3")
 yield("/pdfleave")
-yield("/wait 7")
+yield("/wait 1")
+
+while GetCharacterCondition(45) do
+  yield("/wait 1")
+  yield("/echo Transitioning")
+end
 
 goto LoopTest
 
 ::StopLoop::
 if DutyFail < 4 then
   yield("/echo Alex Looping has completed")
-elseif DutyFail == 0 then
+elseif DutyFail >= 4 then
   yield("/echo It seems like it's failed, maybe lag?")
 end
