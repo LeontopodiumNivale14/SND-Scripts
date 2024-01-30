7ac785c43e03e5671ecd2ce2325b9b14f0ba2203
xicemanx95 <103514618+xicemanx95@users.noreply.github.com>
2024-01-30 18:07:15 -0500

    Update Alexander Farming.lua

--- SND Scripts/Alexander Farming.lua
+++ SND Scripts/Alexander Farming.lua
@@ -4,11 +4,16 @@ It's setup to where you should be able to loop it as many time as you want, and 
 Known classes to work: MCH | SMN | RDM | BLM | BRD
 ]]
 
+--Visland Loops
+Alex_Start = "H4sIAAAAAAAACk2PW2vDMAyF/4ueTXCyNJv9VrYO8tDdIVvHKKbVqKG2Rq3uQsh/nxJctjcd6dPRUQ83LiBYmO/xe12vH0FB534+yEdOYF97uKPk2VME28MzWF3oqrmYmaY2Cl7AlrqY1ea8qhWswJrCNGdaV4MoitheyYKCB7f1R3ErCxFL+sSAkadJGxkPbsOd591tpv/3cjgJlXb0dZpIGnF7d/uEf/gUsVSwCMSnwy1jyOV8IrK4P2LiXI/GnfPjt9lxVNd0uKS4zZ8LNjaffMClcHp4G34B1YIamzkBAAA="
+Alex_Chest = "H4sIAAAAAAAACuWS22oDIRRF/+U8G1FHZ0bfQi+Qh/RGYNqUEqSxRIhaMqYXhvn3OonpQPsH7dvZx81muzgdXGlnQMF0az5WfLUABI3+fA3WxxbUYwc3obXRBg+qg3tQDEtRcVlyBA+gKMEl4VIwBEtQE4HrmtSM9kkGb2bnoDgXCO702u5THMUEwTy8GWd8BJXEzEez08+xsXFzPfh/7HK71KrdhPfTS6qT0l70tjWj/dCRIrhwIZpTVDQuj9ODI4vbvWljnofgRts4Jg7qMuzOgl/nr5PjcmGdmScf6dEvMBOCKZGEympkIzgvhTyykVhIWovqX7IpcCHSoaQGGYwsWPF9NJVgNWV/H8xT/wWlwgmYbwMAAA=="
+
 -- How many loops do you wanna do
 NumberofLoops = 10
 
--- This is just the loop counter itself, keeps tracks of how many you've done.
-CurrentLoop = 1
+-- Values that are needed for the whole script
+CurrentLoop = 1 -- This is just the loop counter itself, keeps tracks of how many you've done.
+DutyCounter = 0
 
 ::LoopTest::
 if NumberofLoops >= CurrentLoop then
@@ -17,17 +22,49 @@ elseif NumberofLoops < CurrentLoop then
   goto StopLoop
 end
 
-
 ::DutyFinder::
-yield("/visland resume")
-yield("/visland stop")
-yield("/dutyfinder")
-yield("/wait 1")
+if DutyFail == 4 then
+  goto StopLoop
 
-yield("/pcall ContentsFinder True 12 0")
+elseif DutyCounter == 0 then
+  yield("/visland resume")
+  yield("/visland stop")
+  yield("/dutyfinder")
+  yield("/wait 0.2")
 
-yield("/wait 1")
+-- Setting up Unsync if it wasn't already
+  yield("/pcall ContentsFinder True 15")
+  yield("/wait 0.3")
+  yield("/pcall ContentsFinderSettings True 1 1 1")
+  yield("/wait 0.5")
+  yield("/pcall ContentsFinderSetting True 0")
+  yield("/wait 0.2")
 
+  yield("/pcall ContentsFinder True 12 1") -- Clearing out any old duties
+  yield("/wait 0.5")
+  OpenRegularDuty(115)
+  yield("/send NUMPAD0")
+  yield("/send NUMPAD0")
+
+  yield("/pcall ContentsFinder True 12 0")
+  DutyCounter = DutyCounter + 1
+  yield("/wait 3.0")
+elseif DutyCounter == 1 then
+  yield("/visland resume")
+  yield("/visland stop")
+  yield("/dutyfinder") --Open Duty Finder
+  yield("/wait 1.0")
+  yield("/pcall ContentsFinder True 12 0") --Duty Load
+  yield("/wait 3.0") 
+elseif DutyCounter == 2 then
+  yield("/echo Hmm... it seems like this has failed, so going to reset it")
+  DutyCounter = 0 
+  DutyFail = DutyFail + 1
+  goto DutyFinder
+end
+
+
+::DutyCommence::
 yield("/click duty_commence")
 yield("/wait 3")
 
@@ -43,7 +80,7 @@ end
 ::BattleInitialize::
 manip_phase = 0
 
-yield("/visland exectemponce H4sIAAAAAAAACk2PW2vDMAyF/4ueTXCyNJv9VrYO8tDdIVvHKKbVqKG2Rq3uQsh/nxJctjcd6dPRUQ83LiBYmO/xe12vH0FB534+yEdOYF97uKPk2VME28MzWF3oqrmYmaY2Cl7AlrqY1ea8qhWswJrCNGdaV4MoitheyYKCB7f1R3ErCxFL+sSAkadJGxkPbsOd591tpv/3cjgJlXb0dZpIGnF7d/uEf/gUsVSwCMSnwy1jyOV8IrK4P2LiXI/GnfPjt9lxVNd0uKS4zZ8LNjaffMClcHp4G34B1YIamzkBAAA=")
+yield("/visland exectemponce "..Alex_Start)
 yield("/wait 0.3")
 
 while IsVislandRouteRunning() do
@@ -64,10 +101,10 @@ end
 yield ("/wait 4")
 
 if (not GetCharacterCondition(26)) then
-  yield("/visland exectemponce H4sIAAAAAAAACuWS22oDIRRF/+U8G9GMl9G30AvMQ3ojMG1KCdJYImS0ZEwvhPn3OhPTKfQP2jf3OZvtduEBrkxjQcNsaz9WbLUABLX5fA3Oxxb04wFuQuuiCx70Ae5BU1wyNaWMIXhIimAuqRQSwRL0hOFScFlw3iUdvK3OQRMEd2bt9imN4iTm4c021sdhU/lod+Y51i5urrP75yyXS6XaTXg/bVKblPZitq0d7UNFiuCiCfF0cRVtk4+zwZHF7d62MZ/74Nq4OCb26jLszoJf55eT43DhGjtPPtKhX1wIpoTLkishvskwJrg6kpFYcSmp+IdkJhSrKVeSF+OXKUSZMPVgeMKklCj+PJin7gumThGobAMAAA==")
+  yield("/visland exectemponce "..Alex_Chest)
 end
 
-yield("/wait 3")
+yield("/wait 2")
 
 while IsMoving() do
   yield("/wait 1")
@@ -82,4 +119,8 @@ yield("/wait 7")
 goto LoopTest
 
 ::StopLoop::
-yield("/echo Alex Looping has completed")
+if DutyFail < 4 then
+  yield("/echo Alex Looping has completed")
+elseif DutyFail == 0 then
+  yield("/echo It seems like it's failed, maybe lag?")
+end
