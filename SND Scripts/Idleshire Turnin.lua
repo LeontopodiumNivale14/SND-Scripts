12e572bc7e3db31110052a58c5f40220c6715bc8
xicemanx95 <103514618+xicemanx95@users.noreply.github.com>
2024-02-01 12:23:52 -0500

    Update Idleshire Turnin.lua

--- SND Scripts/Idleshire Turnin.lua
+++ SND Scripts/Idleshire Turnin.lua
@@ -9,7 +9,7 @@ This requires:
   -> Lifestream 
   -> Deliveroo
 
-  Version: 1.00 [Finally Released]
+  Version: 2.00 [Finally Released]
   it's currently setup to constantly transfer/get the gear until you run out of all mats to be able to GET gear.
   If there's an issue, just ping me in the discord (LegendofIceman)
 ]]
