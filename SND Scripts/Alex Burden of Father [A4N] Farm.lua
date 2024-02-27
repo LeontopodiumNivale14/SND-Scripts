c6ca5176053511666c11a3b9eba054e9bd141f69
xicemanx95 <103514618+xicemanx95@users.noreply.github.com>
2024-02-27 13:14:42 -0500

    V3.3 Update

--- SND Scripts/Alex Burden of Father [A4N] Farm.lua
+++ SND Scripts/Alex Burden of Father [A4N] Farm.lua
@@ -7,7 +7,8 @@
   This is meant to be used for Alexander - The Burden of the Father (NORMAL NOT SAVAGE)
   It's setup to where you should be able to loop it as many time as you want, and be able to farm mats for GC seals
   Known classes to work: ALL
-  Version: 3.2
+  Version: 3.3
+    -> 3.3: Repair Functionality & Potentional duty load check (@leaf update)
   Created by: Ice, Class Support: Ellipsis | Menu Optimizing: Leaf
 
   Creators note: thank you Ellipsis for getting all the classes working, you did an amazing job. You deserve the credit here.
@@ -31,19 +32,23 @@
   **************
   ]]
 
-  -- How many loops do you wanna do
-  NumberofLoops = 5
-  rate = 0.3 --Increase this at lower fps
+  NumberofLoops = 5  -- How many loops do you wanna do
+  rate = 0.3 -- Increase this at lower fps [0.3 works on 15fps+]
+  timeoutThreshold = 15 -- Number of seconds to wait before timeout
 
-  -- if you don't want to deal with the duty selection process, and select the duty yourself/turned on Unsync set this to true. If you want to just automate to the duty, set this to false. 
+  ManualSetDuty = false -- true | false option
+  -- if you don't want to deal with the duty selection process, and select the duty yourself/turned on Unsync set this to true. If you want to just automate to the duty, set this to false.
   -- This assumes that you have EVERYTHING unlocked
-  ManualSetDuty = false
 
+  DutyFinderOrder = true -- true | false option
   -- If you have your duty from 50 at top, and 90 toward the bottom, leave this as true
   -- If you have your duty from 90 at top, and 50 toward the bottom, change this to false
-  DutyFinderOrder = true
 
-  CastingDebug = false
+  CastingDebug = false -- true | false option
+  -- Just something for me to debug test w/
+
+  ManualRepair = true -- if you want to repair between the loops that you do. [defaults is true | off is false]
+  RepairAmount = 75 -- lowest point your gear will 
 
 --[[
 
@@ -74,6 +79,25 @@ elseif NumberofLoops < CurrentLoop then
     goto StopLoop
 end
 
+-- Repair Functionality
+if ManualRepair == true then
+  if NeedsRepair(99) then
+    while not IsAddonVisible("Repair") do
+      yield("/generalaction repair")
+      yield("/wait 0.5")
+    end
+    yield("/pcall Repair true 0")
+    yield("/wait 0.1")
+    if IsAddonVisible("SelectYesno") then
+      yield("/pcall SelectYesno true 0")
+      yield("/wait 0.1")
+    end
+    while GetCharacterCondition(39) do yield("/wait 1") end
+    yield("/wait 1")
+    yield("/pcall Repair true -1")
+  end
+end
+
 ::DutyFinder::
 if DutyFail == 4 then
     goto StopLoop
@@ -99,25 +123,40 @@ elseif DutyCounter == 0 then -- Initially setting up duty to loop Alexander - Bu
         yield("/wait "..rate)
     until GetNodeText("ContentsFinder", 15) == "0/5 Selected"
     OpenRegularDuty(115)
+    local timeout = 0
     repeat
         yield("/wait "..rate)
+        timeout = timeout + 1
+        if timeout > timeoutThreshold / rate then goto NOTALLUNLOCK end
     until GetNodeText("JournalDetail", 19) == "Alexander - The Burden of the Father"
     if DutyFinderOrder then
-      yield("/pcall ContentsFinder True 3 27") 
+      yield("/pcall ContentsFinder True 3 27")
     else
       yield("/pcall ContentsFinder True 3 75")
     end
+    timeout = 0
     repeat
         yield("/wait "..rate)
+        timeout = timeout + 1
+        if timeout > timeoutThreshold / rate then goto NOTALLUNLOCK end
     until GetNodeText("ContentsFinder", 14) == "Alexander - The Burden of the Father"
-
+    
+    ::NOTALLUNLOCK::
+    if GetNodeText("ContentsFinder", 14) ~= "Alexander - The Burden of the Father" then
+        for i = 1, 501 do
+            yield("/pcall ContentsFinder True 3 "..i)
+            yield("/wait "..rate)
+            if GetNodeText("ContentsFinder", 14) == "Alexander - The Burden of the Father" then break end
+        end
+        yield("/wait "..rate)
+    end
 
     yield("/pcall ContentsFinder True 12 0")
     DutyCounter = DutyCounter + 1
     repeat
         yield("/wait "..rate)
     until IsAddonVisible("ContentsFinderConfirm")
-    
+
 elseif DutyCounter == 1 then -- Quicker menu'ing here to load in
     yield("/visland resume")
     yield("/visland stop")
@@ -130,13 +169,15 @@ elseif DutyCounter == 1 then -- Quicker menu'ing here to load in
     while not IsAddonReady("ContentsFinder") do
         yield("/wait "..rate)
     end
+  -- Setting up Unsync if it wasn't already
+    SetDFUnrestricted(true)
     yield("/pcall ContentsFinder True 12 0") --Duty Load
     repeat
         yield("/wait "..rate)
     until IsAddonVisible("ContentsFinderConfirm")
 elseif DutyCounter == 2 then
     yield("/echo Hmm... it seems like this has failed, so going to reset it")
-    DutyCounter = 0 
+    DutyCounter = 0
     DutyFail = DutyFail + 1
     goto DutyFinder
 end
@@ -169,17 +210,17 @@ while not GetCharacterCondition(26) do
             local enemy_y = GetTargetRawYPos()
             local enemy_z = GetTargetRawZPos()
             yield("/visland moveto " .. enemy_x .. " " .. enemy_y .. " " .. enemy_z)
-            yield("/wait "..rate)  
+            yield("/wait "..rate)
             yield("/rotation manual")
         else
             yield("/visland stop")  -- Stop movement after reaching near the target
         end
     end
 end
-  
+
 ::StartofBattle::
 --rotation
-while GetCharacterCondition(26) do 
+while GetCharacterCondition(26) do
     yield("/wait "..rate)
  -- Target selection and movement logic
     local current_target = GetTargetName() --Leaf: need find the setting to always zoom out or something
@@ -195,9 +236,9 @@ while GetCharacterCondition(26) do
       if CastingDebug == true then
         yield("/e EY WATCH IT. I'M CASTING")
       end
-      yield("/wait 0.1")
+      yield("/wait "..rate/3)
     end
-    
+
     local enemy_max_dist = 40
     local dist_to_enemy = GetDistanceToTarget()
     if dist_to_enemy and dist_to_enemy > 0 then
@@ -208,7 +249,7 @@ while GetCharacterCondition(26) do
             yield("/visland moveto " .. enemy_x .. " " .. enemy_y .. " " .. enemy_z)
             yield("/wait "..rate)
         else
-            yield("/visland stop")  -- Stop movement after reaching near the target        
+            yield("/visland stop")  -- Stop movement after reaching near the target
         end
     end
 end
@@ -232,7 +273,7 @@ yield("/pdfleave")
 
 repeat
     yield("/wait "..rate)
-until not IsInZone(445) and not GetCharacterCondition(45)
+until not IsInZone(445) and not GetCharacterCondition(34) and not GetCharacterCondition(56) and not GetCharacterCondition(45)
 
 goto LoopTest
 
