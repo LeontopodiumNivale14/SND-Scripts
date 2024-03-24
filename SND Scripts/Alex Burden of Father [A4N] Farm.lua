58a705d52b945508b744eb4a9d70c5d37bc7eb50
Leontopodium Nivale <natebren95@gmail.com>
2024-03-24 15:20:05 -0400

    Navmesh Version Update

--- SND Scripts/Alex Burden of Father [A4N] Farm.lua
+++ SND Scripts/Alex Burden of Father [A4N] Farm.lua
@@ -1,5 +1,26 @@
 --[[
 
+  ****************************************
+  * Alexander - The Burden of the Father * 
+  *            Normal Farm               *
+  ****************************************
+
+  ***********
+  * Version *
+  *  3.3.4  *
+  ***********
+
+  -> 3.3.4: Switched over to PURELY Navmesh on this, visland is causing some memory leak or SOMETHING atm. So... to avoid further issues/make it to where it works properly, implimenting it (pretty much as seemless, I get peeps don't like change but)
+              ALSO. If you use "YesAlready" and have it to where the setting "ContentsFinderConfirm" is checkmarked, please enable that in the settings tab to true, that way you can keep it on and not have to worry about toggling it. 
+  -> 3.3.3: Added the fucking manual like the main repo. 
+  -> 3.3.2: Added the ability to Infinite Loop w/o having to set a number
+  -> 3.3.1: Added some checks to wait till you're fully loaded out (in case of high ping) [Chest fix is next on the list for high ping]
+  -> 3.3.0: Repair Functionality & Potentional duty load check (@leaf update)
+  Created by: Leontopodium Nivale, Class Support: Ellipsis | Menu Optimizing/tweaks: Leaf
+
+  Creators note: thank you Ellipsis for getting all the classes working, you did an amazing job. You deserve the credit here.
+                 also @Leaf thanks for tweaking it and making this more friendly for situations I didn't account for, you're the best 
+
   ***************
   * Description *
   ***************
@@ -7,16 +28,6 @@
   This is meant to be used for Alexander - The Burden of the Father (NORMAL NOT SAVAGE)
   It's setup to where you should be able to loop it as many time as you want, and be able to farm mats for GC seals
   Known classes to work: ALL
-  Version: 3.3.3 
-    -> 3.3.3: Added the fucking manual like the main repo. 
-    -> 3.3.2: Added the ability to Infinite Loop w/o having to set a number
-    -> 3.3.1: Added some checks to wait till you're fully loaded out (in case of high ping) [Chest fix is next on the list for high ping]
-    -> 3.3.0: Repair Functionality & Potentional duty load check (@leaf update)
-  Created by: Leontopodium Nivale, Class Support: Ellipsis | Menu Optimizing/tweaks: Leaf
-
-  Creators note: thank you Ellipsis for getting all the classes working, you did an amazing job. You deserve the credit here.
-                 also @Leaf thanks for tweaking it and making this more friendly for situations I didn't account for, you're the best 
-
 
   *********************
   *  Required Plugins *
@@ -24,12 +35,12 @@
 
 
   Plugins that are used are:
-  -> Visland (for pathing) : https://puni.sh/api/repository/veyn
+  -> VNavmesh (for pathing) : https://puni.sh/api/repository/veyn
   -> Pandora (Setting "Open Chest") : https://love.puni.sh/ment.json
   -> RotationSolver : https://puni.sh/api/repository/croizat
    -> Something Need Doing [Expanded Edition] : https://puni.sh/api/repository/croizat
     -> In the SND window, press the question mark to make the help setting's menu open 
-    -> Go to options tab -> /target -> DISABLE THIS!! "Stop macro if target not found (only applies to SND's targeting system')"
+    -> Go to options tab -> /target -> DISABLE THIS!! " Stop macro if target not found (only applies to SND's targeting system') "
 ]]
 
 --[[
@@ -39,11 +50,15 @@
   **************
   ]]
 
+  UserName = "" --[[ this is just a quick way to set my preset settings here, so I don't have to go through and re-edit all of the baseline ones that the script defaults to whenever copy/pasting it from the github. 
+                     If you want to use my preferred values, go ahead. Not gonna stop you (not that I could)
+                     Just moreso got tired of re-tping things XD But I also know peeps don't like having excess things at times 
+                  ]]
+
   NumberofLoops = 5 -- number of loops you would like to do 
   InfiniteLoops = false -- options: true | false 
   -- If you want it to continually loop w/o a cap, change InfiniteLoops to true 
   -- this will ignore the number of loops and continually go w/o stopping
-
 
   rate = 0.3 -- Increase this at lower fps [0.3 works on 15fps+]
   timeoutThreshold = 15 -- Number of seconds to wait before timeout
@@ -56,16 +71,20 @@
   -- If you have your duty from 50 at top, and 90 toward the bottom, leave this as true
   -- If you have your duty from 90 at top, and 50 toward the bottom, change this to false
 
-  CastingDebug = false -- true | false option
-  -- Just something for me to debug test w/
+  YesAlreadyContentCheckBox = false -- true | false option 
+  -- If you have it to where you have the setting to automatically confirm in "YesAlready" set this to true. 
+  -- This will make it to where it won't hang on trying to get the duty confirm window to load, and press the duty commence button
+  -- I could pause yes already, but that's a headache I don't wanna think about at this second lol. 
+  -- default is false
 
   ManualRepair = false -- if you want to repair between the loops that you do. [defaults is false | on is true]
-  RepairAmount = 75 -- lowest point your gear will 
+  RepairAmount = 99 -- lowest point your gear will 
 
   EchoHowMany = true -- Would you like to know where in the script the loop is at? [default is true | off is false]
   TrueLoop = false -- would you like to know how many loops you're currently at actually? (tracks how many bolts/things you have) [default is false | on is true]
 
-  CastingDebug = false
+  CastingDebug = false -- true | false option
+  -- Just something for me to debug test w/
 
 --[[
 
@@ -75,6 +94,7 @@
   ************
 
 ]]
+
 -- functions
   function TargetNearestObjectKind(objectKind, radius, subKind)
     local smallest_distance = 10000000000000.0
@@ -104,10 +124,18 @@
       yield("/wait "..rate)
     until IsPlayerAvailable()
   end
-    
 
---Visland Loops
-  Alex_Chest = "H4sIAAAAAAAACuWQSWvDMBCF/0qZsyMkR7It3UIX8CHdCLgLJYhkTASxVWy5C8b/vYpj40ALvRZ605t5enr6WrjWBYKCxR4/1ny9ggAy/flqTelqUM8t3NraOGNLUC08gAqJFDGXEQ/gERSjJKJcijCAJ1AzQZKEJiHrvLQlphegaAD3emsaH8aIF0v7hgWWrt+kpcNKb1xm3O5mcJ/Ohm6+U72z7+PGl/Fpud7XONn7hiyAy8K68eHUYTEcF71jEHcN1m44H4IzbdyUeFBXtjq35Xb4OD0OV6bApffRLviGZUYJo5IyGU9kBOeRkEcykgjJEhH/QzIhoaHkyURlLrk4UonIPJpTmZxQ4YfdyMVf/Y0Ljz3hH8i4CnXdVHi2sXmO1Z8D9dJ9Ad/rgrl7AwAA"
+    if UserName == "Ice" then 
+        InfiniteLoops = true
+        ManualSetDuty = true
+        DutyFinderOrder = true
+        YesAlreadyContentCheckBox = false
+        ManualRepair = true
+        RepairAmount = 99
+        EchoHowMany = true
+        TrueLoop = true
+    end
+
 
 -- custom stuff for me to be insane, ignore This
   LensID = 12674
@@ -128,12 +156,12 @@
 
 
 -- Values that are needed for the whole script
-  CurrentLoop = 1 -- This is just the loop counter itself, keeps tracks of how many you've done.
-  if ActualLoopCount > CurrentLoop and TrueLoop == true then 
-    CurrentLoop = ActualLoopCount
-  end
-  DutyCounter = 0
-  DutyFail = 0
+    CurrentLoop = 1 -- This is just the loop counter itself, keeps tracks of how many you've done.
+    if ActualLoopCount > CurrentLoop and TrueLoop == true then 
+        CurrentLoop = ActualLoopCount
+    end
+    DutyCounter = 0
+    DutyFail = 0
 
 
 if ManualSetDuty == true then
@@ -195,7 +223,7 @@ if DutyFail == 4 then
     goto StopLoop
 
 elseif DutyCounter == 0 then -- Initially setting up duty to loop Alexander - Burden of the Father (A4N)
-    yield("/visland stop")
+    PathStop()
     --Open duty finder until it's visible
     while not IsAddonVisible("ContentsFinder") do
         yield("/dutyfinder")
@@ -222,9 +250,9 @@ elseif DutyCounter == 0 then -- Initially setting up duty to loop Alexander - Bu
         if timeout > timeoutThreshold / rate then goto NOTALLUNLOCK end
     until GetNodeText("JournalDetail", 19) == "Alexander - The Burden of the Father"
     if DutyFinderOrder then
-      yield("/pcall ContentsFinder True 3 27")
+        yield("/pcall ContentsFinder True 3 27")
     else
-      yield("/pcall ContentsFinder True 3 75")
+        yield("/pcall ContentsFinder True 3 75")
     end
     timeout = 0
     repeat
@@ -245,13 +273,12 @@ elseif DutyCounter == 0 then -- Initially setting up duty to loop Alexander - Bu
 
     yield("/pcall ContentsFinder True 12 0")
     DutyCounter = DutyCounter + 1
-    repeat
+    while IsAddonVisible("ContentsFinderConfirm") == false and YesAlreadyContentCheckBox == false do 
         yield("/wait "..rate)
-    until IsAddonVisible("ContentsFinderConfirm")
+    end 
 
 elseif DutyCounter == 1 then -- Quicker menu'ing here to load in
-    yield("/visland resume")
-    yield("/visland stop")
+    PathStop()
     --Open duty finder until it's visible
     while not IsAddonVisible("ContentsFinder") do
         yield("/dutyfinder")
@@ -262,9 +289,9 @@ elseif DutyCounter == 1 then -- Quicker menu'ing here to load in
         yield("/wait "..rate)
     end
     yield("/pcall ContentsFinder True 12 0") --Duty Load
-    repeat
+    while IsAddonVisible("ContentsFinderConfirm") == false and YesAlreadyContentCheckBox == false do 
         yield("/wait "..rate)
-    until IsAddonVisible("ContentsFinderConfirm")
+    end 
 elseif DutyCounter == 2 then
     yield("/echo Hmm... it seems like this has failed, so going to reset it")
     DutyCounter = 0
@@ -282,7 +309,7 @@ until IsInZone(445) and IsAddonVisible("_Image")
 ::BattleInitialize::
 manip_phase = 0
 while not GetCharacterCondition(26) do
--- Target selection and movement logic
+    -- Target selection and movement logic
     local current_target = GetTargetName()
     if not current_target or current_target == "" then
         yield("/targetenemy")  -- Attempt to auto-target the next enemy
@@ -292,18 +319,22 @@ while not GetCharacterCondition(26) do
         end
     end
 
-    local enemy_max_dist = 40
-    local dist_to_enemy = GetDistanceToTarget()
-    if dist_to_enemy and dist_to_enemy > 0 then
-        if dist_to_enemy <= enemy_max_dist then
+    local enemy_max_dist = 75
+    if GetDistanceToTarget() > 3 then
+        if GetDistanceToTarget() <= enemy_max_dist then
             local enemy_x = GetTargetRawXPos()
             local enemy_y = GetTargetRawYPos()
             local enemy_z = GetTargetRawZPos()
-            yield("/visland moveto " .. enemy_x .. " " .. enemy_y .. " " .. enemy_z)
+            PathfindAndMoveTo(enemy_x, enemy_y, enemy_z)
+            while PathfindInProgress() do 
+                yield("/wait 0.05")
+            end
             yield("/wait "..rate)
             yield("/rotation manual")
-        else
-            yield("/visland stop")  -- Stop movement after reaching near the target
+            while GetDistanceToTarget() > 3 do 
+                yield("/wait 0.05")
+            end
+            PathStop()  -- Stop movement after reaching near the target
         end
     end
 end
@@ -312,7 +343,7 @@ end
 --rotation
 while GetCharacterCondition(26) do
     yield("/wait "..rate)
- -- Target selection and movement logic
+    -- Target selection and movement logic
     local current_target = GetTargetName() --Leaf: need find the setting to always zoom out or something
     if not current_target or current_target == "" then
         yield("/targetenemy")  -- Attempt to auto-target the next enemy
@@ -323,23 +354,28 @@ while GetCharacterCondition(26) do
     end
 
     while IsPlayerCasting() do
-      if CastingDebug == true then
-        yield("/e EY WATCH IT. I'M CASTING")
-      end
-      yield("/wait "..rate/3)
+        if CastingDebug == true then
+            yield("/e EY WATCH IT. I'M CASTING")
+        end
+        yield("/wait "..rate/3)
     end
 
-    local enemy_max_dist = 40
-    local dist_to_enemy = GetDistanceToTarget()
-    if dist_to_enemy and dist_to_enemy > 0 then
-        if dist_to_enemy <= enemy_max_dist then
+    local enemy_max_dist = 75
+    if GetDistanceToTarget() > 3 then
+        if GetDistanceToTarget() <= enemy_max_dist then
             local enemy_x = GetTargetRawXPos()
             local enemy_y = GetTargetRawYPos()
             local enemy_z = GetTargetRawZPos()
-            yield("/visland moveto " .. enemy_x .. " " .. enemy_y .. " " .. enemy_z)
+            PathfindAndMoveTo(enemy_x, enemy_y, enemy_z)
+            while PathfindInProgress() do 
+                yield("/wait 0.05")
+            end
             yield("/wait "..rate)
-        else
-            yield("/visland stop")  -- Stop movement after reaching near the target
+            yield("/rotation manual")
+            while GetDistanceToTarget() > 3 do 
+                yield("/wait 0.05")
+            end
+            PathStop()  -- Stop movement after reaching near the target
         end
     end
 end
@@ -347,19 +383,49 @@ end
 -- if the enemy is beyond the max distance, depending on your needs.
 
 yield("/rotation cancel")
-yield("/visland exectemponce "..Alex_Chest)
 
-repeat
-    yield("/wait "..rate)
-until IsMoving()
+-- Chest #1
+    PathfindAndMoveTo(1.93,10.60,-6.31)
+    while PathfindInProgress() do
+        yield("/wait 0.05")
+    end
+    while PathIsRunning() do 
+        yield("/wait 0.05")
+    end 
+    yield('/target "Treasure Coffer"')
+    yield("/pint")
 
-repeat
-    yield("/wait "..rate)
-until not IsVislandRouteRunning()
+-- Chest #2
+    PathfindAndMoveTo(-0.15,10.54,-8.23)
+    while PathfindInProgress() do
+        yield("/wait 0.05")
+    end
+    while PathIsRunning() do 
+        yield("/wait 0.05")
+    end 
+    yield('/target "Treasure Coffer"')
+    yield("/pint")
+
+-- Chest #3
+    PathfindAndMoveTo(-2.18,10.57,-6.41)
+    while PathfindInProgress() do
+        yield("/wait 0.05")
+    end
+    while PathIsRunning() do 
+        yield("/wait 0.05")
+    end 
+    yield('/target "Treasure Coffer"')
+    yield("/pint")
 
 while TargetNearestObjectKind(4) do
-    if not IsVislandRouteRunning() then
-        yield("/visland moveto " .. GetTargetRawXPos() .. " " .. GetTargetRawYPos() .. " " .. GetTargetRawZPos())
+    if PathIsRunning() == false then
+        ChestX = GetTargetRawXPos()
+        ChestY = GetTargetRawYPos()
+        ChestZ = GetTargetRawZPos()
+        PathfindAndMoveTo(ChestX, ChestY, ChestZ)
+        while PathfindInProgress() do 
+            yield("/wait 0.05")
+        end 
     end
     yield("/wait "..rate)
 end
