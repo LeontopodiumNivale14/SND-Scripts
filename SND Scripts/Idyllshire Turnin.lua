a1913224acb91896b68c1bcdfbf4a4f62c97c974
Leontopodium Nival <natebren95@gmail.com>
2024-04-12 01:05:38 -0400

    Update Idyllshire Turnin.lua

--- SND Scripts/Idyllshire Turnin.lua
+++ SND Scripts/Idyllshire Turnin.lua
@@ -5,13 +5,16 @@
     *   Alex 1-4 Edition  *
     ***********************
 
+    READ THIS PLEASE: There seems to be some sort of bug that's causing the script to not... copy properly? If you're using the github copy button? It's like 1AM as i'm typing this so. Dont' know the bug/reason.
+    Just make sure to select the whole script with your mouse (or even by opening the raw version) and then proceeding to paste it in SND. 
+
     **************
     *   VERSION  *
-    *   3.3.2.I  *
+    *   3.3.2.J  *
     **************
 
     Update notes:
-        3.3.2.G -> THIS IS ME TESTING. I WILL THROW YOU IF YOU TELL ME THAT IT'S BROKE ATM LOL
+        3.3.2.J -> THIS IS ME TESTING. I WILL THROW YOU IF YOU TELL ME THAT IT'S BROKE ATM LOL
         3.3.1.11  -> Removed Old GC Ticket teleport system, since that's just baked into the GCTeleport itself. Took that time to also re-write that whole section and clean it up a bit.
         3.3.1.10  -> Made some tiny optimizations that's been bugging me, nothing to major. mounting in Idyllshire for instance...
         3.3.1.0   -> Small fix to if you're buying in gridania, added a waypoint so you wouldn't get stuck on the step trying to turn it in
