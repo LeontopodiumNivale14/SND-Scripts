7e24f90bd3c0039d26e126219a4714de17972929
Leontopodium Nival <natebren95@gmail.com>
2024-04-15 01:10:35 -0400

    Update Alex Burden of Father [A4N] Farm.lua

--- SND Scripts/Alex Burden of Father [A4N] Farm.lua
+++ SND Scripts/Alex Burden of Father [A4N] Farm.lua
@@ -163,6 +163,14 @@
 --Visland Loops
     Alex_Chest = "H4sIAAAAAAAACuWQSWvDMBCF/0qZsyMkR7It3UIX8CHdCLgLJYhkTASxVWy5C8b/vYpj40ALvRZ605t5enr6WrjWBYKCxR4/1ny9ggAy/flqTelqUM8t3NraOGNLUC08gAqJFDGXEQ/gERSjJKJcijCAJ1AzQZKEJiHrvLQlphegaAD3emsaH8aIF0v7hgWWrt+kpcNKb1xm3O5mcJ/Ohm6+U72z7+PGl/Fpud7XONn7hiyAy8K68eHUYTEcF71jEHcN1m44H4IzbdyUeFBXtjq35Xb4OD0OV6bApffRLviGZUYJo5IyGU9kBOeRkEcykgjJEhH/QzIhoaHkyURlLrk4UonIPJpTmZxQ4YfdyMVf/Y0Ljz3hH8i4CnXdVHi2sXmO1Z8D9dJ9Ad/rgrl7AwAA"
 
+    mob_table = 
+        {
+            {"Right Foreleg"},
+            {"Left Foreleg"},
+            {"The Manipulator"},
+            {"Panzer Doll"},
+        }
+
 -- Values that are needed for the whole script
     CurrentLoop = 1 -- This is just the loop counter itself, keeps tracks of how many you've done.
     if ActualLoopCount > CurrentLoop and TrueLoop == true then 
